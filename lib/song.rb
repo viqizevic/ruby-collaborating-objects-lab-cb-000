fee7ef29c46abd95d2473bac655faca024127530
Vicky Tanzil <Vicky.Tanzil@parexel.com>
2018-09-25 12:34:54 +0200

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,12 +6,16 @@ class Song
     @name = name
   end
 
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    @artist = artist
+    artist.add_song(self)
+  end
+
   def self.new_by_filename(filename)
     song_file = File.basename(filename, '.mp3').split(' - ')
     song = self.new(song_file[1])
-    artist = Artist.find_or_create_by_name(song_file[0])
-    song.artist = artist
-    artist.add_song(song)
+    song.artist_name = song_file[0]
     song
   end
 end
